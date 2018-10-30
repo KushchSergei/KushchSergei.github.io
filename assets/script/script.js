4b4e5a02f8fd888d0eb476fdcf00e941d9618b2f
Serejka <yoshkinkot14042000@gmail.com>
2018-10-30 23:10:45 +0200

    init

---
+++ assets/script/script.js
@@ -0,0 +1,416 @@
+var messages = [
+	{
+        "id": "1",
+        "theme": "Племінний склад хунну та гуннів",
+        "author": "JmiSHENKO",
+        "text": "Територія культури простягалася трохи ширше, ніж у попередньої культури Нижнього шару Сяцзядянь, досягаючи територій на"+
+        " північ від річки Сямулун. У порівнянні з культурою Нижнього шару Сяцзядянь, чисельність населення була нижчою, воно було менш щільним,"+
+        " однак населяло більшу територію. Як і попередня культура, дана все ще сильно залежала від землеробства, проте відбулося деяке зміщення "+
+        "до пастушескому, кочового способу життя. Соціальна структура змінилася: на зміну племінному спільноті без централізованої влади прийшла "+
+        "система вождества. Типовим пам'ятником є ​​верхній шар Сяцзядяня в окрузі Чіфен, Внутрішня Монголія."+
+        "Керамічні вироби даної культури були гірші за якістю, ніж вироби попередньої культури Нижнього шару Сяцзядянь, хоча це компенсувалося "+
+        "кращими виробами з бронзи, кістки і каменю. Характерними для даної культури є бронзові вироби: кинджали, сокири, стамески, наконечники стріл, ножі і шоломи. Всі ці бронзові вироби, як правило, були прикрашені тваринами і природними мотивами, в яких простежується схожість зі скіфськими, що свідчить про контакти степових культур Євразії [2].",
+        "date": "04:44 | 3 октября 2018"
+    }
+]
+
+var section = [
+	{
+		"title": "Фільми",
+        "description": " якийсь опис ",
+        "src": "images/themes/1.jpg"
+	},
+	{   
+		"title": "Історія",
+        "description": " якийсь опис ",
+        "src": "images/themes/2.jpg"
+	},
+	{
+		"title": "Музика",
+        "description": " якийсь опис ",
+        "src": "images/themes/3.jpg"
+	},
+	{
+		"title": "Серіали",
+        "description": " якийсь опис ",
+        "src": "images/themes/4.jpg"
+	},
+	{
+		"title": "Космос",
+        "description": " якийсь опис ",
+        "src": "images/themes/5.jpg"
+	},
+	{
+		"title": "Ігри",
+        "description": " якийсь опис ",
+        "src": "images/themes/6.jpg"
+	},
+	{
+		"title": "Моб. пристрої",
+        "description": " якийсь опис ",
+        "src": "images/themes/7.jpg"
+	},
+	{
+		"title": "Комп. комплуктуючі",
+        "description": " якийсь опис ",
+        "src": "images/themes/8.jpg"
+    },
+	{
+		"title": "Програмування",
+        "description": " якийсь опис ",
+        "src": "images/themes/9.jpg"
+    },
+	{
+		"title": "Програми",
+        "description": " якийсь опис ",
+        "src": "images/themes/10.jpg"
+    },
+	{
+		"title": "Анімація",
+        "description": " якийсь опис ",
+        "src": "images/themes/11.jpg"
+    },
+	{
+		"title": "Меми",
+        "description": " якийсь опис ",
+        "src": "images/themes/12.jpg"
+    }
+]
+
+var theme_list = [
+    {
+        "section" : "Програмування",
+        "theme_title": "Как создать проект на angular2 если глобально установлен Angular 6?",
+        "theme_description": "Приложение должно быть с интерфейсом командной строки(CLI). Пробовал устанавливать cli@1.0.0 - в итоге проект был на angular 4.4.7, можно как-то установить второй, может даже с удалением глобальной версии.",
+        "theme_author": "Sanechek",
+        "theme_date": "21:00 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Програмування",
+        "theme_title": "Несколько JQuery Slider на странице",
+        "theme_description": "Подскажите пожалуйста, подключил jqueryui.com/slider на страницу , задал свои настройки, все работает. Он у меня дублируется в трех местах, при копипасте в новом месте он уже не отображается. Подскажите как решить проблему без подключения дополнительных слайдеров? Заранее спасибо!",
+        "theme_author": "JMISHENKO",
+        "theme_date": "12:28 | 2 октября 2018",
+        "answ_num": 3
+    },
+    {
+        "section" : "Програмування",
+        "theme_title": "Многопоточность в С++. В чем отличие pthread от std::thread?",
+        "theme_description": "В чем основные отличия posix threads и класса std::thread? В каком случае что выбрать и почему? Желательно, поподробнее.",
+        "theme_author": "Spasite",
+        "theme_date": "04:44 | 3 октября 2018",
+        "answ_num": 2
+    },
+    {
+        "section" : "Програмування",
+        "theme_title": "Сколько живут cookie, если явно не задавать срок жизни?",
+        "theme_description": "Теоретически, если не задать явно время жизни cookie, она должна жить, пока жив экземпляр браузера (пока не будут закрыты все окна и вкладки).",
+        "theme_author": "Vladik_Vladik",
+        "theme_date": "08:00 | 3 октября 2018",
+        "answ_num": 7
+    },
+    {
+        "section" : "Програмування",
+        "theme_title": "Зачем точка в JavaScript Ex: value.countReload = 1;",
+        "theme_description": "Недавно видел в примерах JS точку, но никак не могу понять зачем она, ли то для изменения переменных внутри функций из вне, или еще чего, прошу извинения за свою некомпетентность, и возможно глупый вопрос. Пример: value.countReload = 1",
+        "theme_author": "Sergei_Kushch",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Фільми",
+        "theme_title": "Якась тема з розділу фільми",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Історія",
+        "theme_title": "Якась тема з розділу історія",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Музика",
+        "theme_title": "Якась тема з розділу музика",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Серіали",
+        "theme_title": "Якась тема з розділу серіали",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Космос",
+        "theme_title": "Якась тема з розділу космос",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Ігри",
+        "theme_title": "Якась тема з розділу ігри",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Моб. пристрої",
+        "theme_title": "Якась тема з розділу Моб. пристрої",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Комп. комплуктуючі",
+        "theme_title": "Якась тема з розділу Комп. комплуктуючі",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Програми",
+        "theme_title": "Якась тема з розділу Моб. Програми",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Анімація",
+        "theme_title": "Якась тема з розділу Моб. Анімація",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Меми",
+        "theme_title": "Якась тема з розділу Моб. пристрої",
+        "theme_description": "Lorem ipsum bibendum elementum eu enim mauris curabitur quam nam arcu ipsum, elementum congue arcu non nec lectus. Maecenas sit commodo quam nec ultricies et, donec sem, proin eu at bibendum. Nec mauris vivamus proin, mauris lorem rutrum: ut pellentesque porttitor tellus massa non quam eu integer ultricies",
+        "theme_author": "Деякий користувач",
+        "theme_date": "23:05 | 2 октября 2018",
+        "answ_num": 1
+    },
+    {
+        "section" : "Історія",
+        "theme_title": "Племінний склад хунну та гуннів",
+        "theme_description": "Де знайти список 24 племен хунну? Чи є таке взагалі? Хтось проводив розбір китайських ієрогліфів, транскрипцію,"+
+        " спроби проводити паралелі з наступними етноніму? До чого ці мої пошуки. Ось є оногури, савіри, кутригури, булгари, сарогури і тд."+
+        " Вони є в візантійських джерелах і населяють Кавказ і причорноморря. Вони з'явилися відразу після хунну. І ось, чому б просто не "+
+        "припустити що це хунну? Тобто їх частини? Я про це підозрював давно, але ніде про це не пишуть, хоча все веде до цього."+
+        " Чому вся їх історія починається всюди з згадки в європейських джерелах, адже легко ототожнити їх з аналогами в китайських джерелах."+
+        " Чому історики так бояться проводити аналогії? Навіть коли вони так очевидні. Загалом, далі будуть крихти зі знайдених мною джерел.",
+        "theme_author": "Зубенко Михаїл Петрович",
+        "theme_date": "23:05 | 2 січня 2018",
+        "answ_num": 3
+    }
+]
+
+const SectionList = {
+    template: `
+    <div id="works"  class=" clearfix grid"> 
+        <figure class="effect-oscar  wowload fadeIn" v-for="(item, index) in items">
+            <img v-bind:src="item.src" alt="img01"/>
+            <figcaption>
+                <h2>{{item.title}}</h2></br>
+                <p>{{item.description}}<br></p>
+                <router-link v-bind:to="'/section/' + item.title"></router-link>        
+            </figcaption>
+        </figure>
+    </div>
+    `,
+    data: function () {
+        return {
+            items: []
+        };
+    },
+    mounted: function(){
+        this.items = section;
+    }
+}
+
+const ThemeList = {
+    template: `
+    <div class="container">
+        <div class="main-content">
+            <div class="row">
+                <div class="col-md-10 col-md-push-2">
+                    <div class="section">
+                        <div class="section-title">Розділ: {{$route.params.title}}</div>
+                        <div class="section-search"><form><input placeholder="Пошук по темам" v-model="search"></form></div>
+                    </div>
+                    <div id="theme_list">
+                        <div class="shortcut" v-for="(theme, index) in filtered_themes">
+                        <router-link v-bind:to="'/themes/' + theme.theme_title">{{theme.theme_title}}</router-link>
+                            <div class="theme-topic">{{theme.theme_description}}</div>
+                            <div class="author">
+                            <a href="#author">{{theme.theme_author}}</a>
+                            <p>Відповідей: {{theme.answ_num}} | {{theme.theme_date}}</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-2 col-md-pull-10">
+                    <div class="sidebar">
+                        <div class="widget">
+                            <ul>
+                                <li><a href="#">Список розділів</a></li>
+                                <li><a href="index.html">Назад</a></li>
+                            </ul>
+                        </div>
+                        <button type="button" class="new_theme">Задати питання</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+    data: function() {
+        return {
+            items: [],
+            search: '',
+            list: null
+        };
+    },
+    mounted: function() {
+        this.items = section;
+        this.list = theme_list.filter((item) => {
+            return (item.section.toUpperCase() === this.$route.params.title.toUpperCase());
+        });
+    },
+    computed: {                                 
+            filtered_themes: function() {
+            if (!this.search) {
+                return this.list;
+            }
+            return this.list.filter(element => {
+                return element.theme_title.toUpperCase().includes(this.search.toUpperCase());
+            });
+        },
+    }
+}
+
+const MessageList = {
+    template: `
+    <div class="container">
+		<div class="main-content">
+				<div class="row">
+                    <div class="col-md-10 col-md-push-2">
+                    <div class="theme">
+						<div class="section">
+                            <div class="theme-title">{{theme.theme_title}}<hr style="margin: 8px 0 5px 0"></div>
+                        </div>
+                            <div class="theme-topic">{{theme.theme_description}}</div>
+                            <div class="author">
+                                <a href="#author">{{theme.theme_author}}</a>
+                                <p>{{theme.theme_date}}</p>
+                            </div>
+                    </div>    
+						<div id="theme_list">
+                            <div class="msg" v-for="(msg, index) in messages">
+                                <div class="vote_pannel">
+
+                                </div>
+                                <div class="msg-c">
+                                    <div class="msg-text">{{msg.text}}</div>
+                                    <div class="msg-data">
+                                        <div class="msg_author">
+                                            <a href="#author">{{msg.author}}</a>
+                                        </div>
+                                        <div class="msg_date">
+                                            <p>{{msg.date}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="inp">
+                                <form style="margin:0;">
+                                    <div class="send_title"><p>Ваша відповідь:</p></div>
+                                    <div><textarea rows="5" v-model="msg_text" name="msg" style="width:100%;"></textarea></div>
+                                    <div><input type="button" v-on:click="send_msg" value="Отправить"></div>
+                                </form>
+                            </div> 
+					</div>
+					<div class="col-md-2 col-md-pull-10">
+						<div class="sidebar">
+							<div class="widget">
+								<ul>
+								<li><a href="#">Список розділів</a></li>
+								<li><a href="index.html">Назад</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+    </div>
+    `,
+    data: function () {
+        return {
+            theme: null,
+            messages: [],
+            msg_text: ''
+        };
+    },
+    methods:{
+            send_msg: function(){
+                let d = new Date();
+                let dat = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+                let msg = new Object;
+                msg.id=Math.floor(Math.random() * (100 - 2 + 1) + 1);
+                msg.theme=this.$route.params.theme_title;
+                msg.author="Kushch";
+                msg.text=this.msg_text;
+                msg.date=dat;
+                messages.push(msg);
+                console.log(messages);                
+            }
+    },
+    beforeMount: function(){
+        this.theme = theme_list.find((item) => {
+            return (item.theme_title.toUpperCase() === this.$route.params.theme_title.toUpperCase());
+        });
+    },
+    mounted: function(){
+        this.messages = messages.filter((item) => {
+            return (item.theme.toUpperCase() === this.$route.params.theme_title.toUpperCase());
+        });
+
+    }
+}
+
+const routes = [
+    { path: '/', component: SectionList },
+    { path: '/section/:title', component: ThemeList },
+    { path: '/themes/:theme_title', component: MessageList }
+]
+
+const router = new VueRouter({
+    routes,
+	scrollBehavior (to, from, savedPosition) {
+	return { x: 0, y: 0 }
+	}
+});
+
+new Vue({
+    el: '#app',
+    router
+});
+
+

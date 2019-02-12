f458cfa9c920c36e9fe0d8f239581190ade6db82
Serejka <yoshkinkot14042000@gmail.com>
2019-02-12 22:04:14 +0200

    calendar

---
+++ precache-manifest.2e483ac0088a5e988a31c60bce8df91f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c770a38f3be9c7d4500c",
+    "url": "/static/js/main.c770a38f.chunk.js"
+  },
+  {
+    "revision": "8cbeffacbb9519a1af68",
+    "url": "/static/js/1.8cbeffac.chunk.js"
+  },
+  {
+    "revision": "c770a38f3be9c7d4500c",
+    "url": "/static/css/main.3ba8f3c2.chunk.css"
+  },
+  {
+    "revision": "5ff7137011811b46526f44bad50077de",
+    "url": "/index.html"
+  }
+];
